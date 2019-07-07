8a863d948db7cc59a4256b30c3eb4918b536e686
John Pier <warhamer400000@yandex.ru>
2019-07-07 19:44:05 +0400

    Removing unused dependencies
    This is "using * "

--- src/Popov N/MySpellChecker/Tests/LogicTests.cs
+++ src/Popov N/MySpellChecker/Tests/LogicTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using MySpellChecker;
 using MySpellChecker.Interfaces;
 using NUnit.Framework;

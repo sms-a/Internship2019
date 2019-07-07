14590209e271cf2a19434326837a10c5bc78f1b1
NesterovaAA <nest30100@gmail.com>
2019-07-07 14:13:23 +0400

    Add some tests

--- src/Nesterova/SpellChecker/SpellChecker/Checker.cs
+++ src/Nesterova/SpellChecker/SpellChecker/Checker.cs
@@ -7,18 +7,54 @@ namespace SpellChecker
     public class Checker
     {
         List<string> dictionary;
-        public Checker (List<string> dictionary)
+        public Checker(List<string> dictionary)
         {
             this.dictionary = dictionary;
         }
         public string GetCorrectWord(string word)
         {
             if (dictionary.Contains(word.ToLower())) return word;
+            string correctWord = FindWordsWihOneEdit(word);
+            if (correctWord.Length > 0) return correctWord;
             return IncorrectWord(word);
         }
         string IncorrectWord(string word)
         {
             return "{" + word + "?}";
         }
-    }
-}
+        List <string> GetWordsWithOneEdit(string word)
+        {
+            List<string> wordsList = new List<string>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                string newWord = word.Substring(0, i) + word.Substring(i + 1, word.Length - i-1);
+                if (dictionary.Contains(newWord)&&!wordsList.Contains(newWord)) wordsList.Add(newWord);
+            }
+            string alpa = "abcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i <= word.Length; i++)
+            {
+                foreach (char letter in alpa)
+                {
+                    string newWord = word.Substring(0, i) + letter+word.Substring(i, word.Length - i);
+                    if (dictionary.Contains(newWord) && !wordsList.Contains(newWord)) wordsList.Add(newWord);
+                }
+            }
+            return wordsList;
+        }
+        string FindWordsWihOneEdit(string word)
+        {
+            string correctWord = "";
+            List<string> wordsWithOneEdit = GetWordsWithOneEdit(word);
+            if (wordsWithOneEdit.Count == 0) return correctWord;
+            if (wordsWithOneEdit.Count > 1)
+            {
+                correctWord = "{ ";
+                foreach (string w in dictionary)
+                    if (wordsWithOneEdit.Contains(w))
+                        correctWord += w + " ";
+                correctWord += "}";
+                return correctWord;
+            }
+            return correctWord = wordsWithOneEdit[0];           
+        }
+    }}
--- src/Nesterova/Tests/Tests.cs
+++ src/Nesterova/Tests/Tests.cs
@@ -15,5 +15,32 @@ namespace Tests
             var checker = new SpellChecker.Checker(dictionary);
             Assert.Equal(word, checker.GetCorrectWord(word));
         }
+        [Fact]
+        public void WordIsNotCorrect()
+        {
+            string word = "sad";
+            List<string> dictionary = new List<string>();
+            dictionary.Add("sadness");
+            var checker = new SpellChecker.Checker(dictionary);
+            string result = "{" + word + "?}";
+            Assert.Equal(result, checker.GetCorrectWord(word));
+        }
+        [Fact]
+        public void WordHaveOneCorrectVariatWithOneEdit()
+        {
+            string word = "sdness";
+            List<string> dictionary = new List<string>(){ "sadness", "july", "empty", "a", "awful" };
+            var checker = new SpellChecker.Checker(dictionary);
+            Assert.Equal("sadness", checker.GetCorrectWord(word));
+        }
+        [Fact]
+        public void WordHaveFewCorrectVariatWithOneEdit()
+        {
+            string word = "sdness";
+            List<string> dictionary = new List<string>() { "sadness", "sness", "sodness", "a", "awful" };
+            var checker = new SpellChecker.Checker(dictionary);
+            Assert.Equal("{ sadness sness sodness }", checker.GetCorrectWord(word));
+        }
+
     }
 }

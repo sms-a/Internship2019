7b1cb6278690ee3fff3c1ae1d61068684958e0bf
John Pier <warhamer400000@yandex.ru>
2019-07-07 19:44:05 +0400

    Removing unused dependencies
    This is "using * "

--- src/Popov N/MySpellChecker/MySpellChecker/CorrectionsLogic.cs
+++ src/Popov N/MySpellChecker/MySpellChecker/CorrectionsLogic.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.CodeDom;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using MySpellChecker.Interfaces;
 
 namespace MySpellChecker

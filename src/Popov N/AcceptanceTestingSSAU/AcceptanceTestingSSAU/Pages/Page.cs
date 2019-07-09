5619a94c7bbba8eb5953c3dfb601631491f694dc
John Pier <warhamer400000@yandex.ru>
2019-07-10 00:18:41 +0400

    Add some files

---
+++ src/Popov N/AcceptanceTestingSSAU/AcceptanceTestingSSAU/Pages/Page.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace AcceptanceTestingSSAU.Pages
+{
+    class Page
+    {
+        private IWebDriver driver;
+
+        public Page(IWebDriver driver, double timeOut = 10.0)
+        {
+            this.driver = driver;
+           // this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(timeOut);
+           // this.driver.Manage().Window.Maximize();
+        }
+
+        public IWebElement FindElementByXPath(string css)
+        {
+            //IWebElement el;
+
+           // return el;
+        }
+
+        public void OpenPage(string pagePath)
+        {
+            //driver.Navigate().GoToUrl(pagePath);
+        }
+
+    }
+}

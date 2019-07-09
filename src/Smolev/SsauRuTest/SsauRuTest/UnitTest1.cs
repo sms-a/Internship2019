171b19bfb945299dec83a7ec4bf7ef02735a7cb3
cjoz <47791266+cjoz@users.noreply.github.com>
2019-07-09 21:39:57 +0400

    Added SSAU website test project

---
+++ src/Smolev/SsauRuTest/SsauRuTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SsauRuTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
